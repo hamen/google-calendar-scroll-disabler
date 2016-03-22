5efb554f547d315d9a8df2ef4fc3d998d94e18b3
Federico Bricker <fbricker@gmail.com>
2016-03-22 11:27:24 -0300

    Fix scrolling when the mouse passes over a line on top of the calendar grid.

--- code/content.js
+++ code/content.js
@@ -4,4 +4,8 @@ $('#gridcontainer').on({
         e.preventDefault();
         e.stopPropagation();
     }
-})
+});
+
+$('#mainbody').css("padding-top","0");
+$('#gridcontainer').css("margin-top","9px");
+$('body').css("overflow","hidden");
