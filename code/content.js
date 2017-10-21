8ea2656efd867880427499aff3cf4908c001550c
Ivan Morgillo <imorgillo@gmail.com>
2017-10-21 16:40:20 +0200

    Update to the new Google Calendar UI

--- code/content.js
+++ code/content.js
@@ -1,7 +1,17 @@
-$('#gridcontainer').on({
-    'mousewheel': function(e) {
-        if (e.target.id == 'el') return;
-        e.preventDefault();
-        e.stopPropagation();
-    }
-})
+// Wait for the first part of the page to load
+$(document).ready(function () {
+
+    // Wait for the rest of the fancy dynamic stuff to load.
+    // 5 seconds should be enough.
+    setTimeout(function () {
+
+        // Get a handle on the calendar grid
+        $('div[role="grid"]').on('mousewheel', function (e) {
+            
+            // Scrolling.... hahhahahaha I don't think so
+            if (e.target.id == 'el') return;
+            e.preventDefault();
+            e.stopPropagation();
+        });
+    }, 5000);
+});
