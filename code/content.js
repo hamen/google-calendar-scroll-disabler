fed7ceec8053bcf7823e3e8674567fa1ee7622aa
Martin Sarsini <martin@apphancer.com>
2018-09-23 16:56:34 +0200

    Add Mutation Observer to detect date change in a specific element

--- code/content.js
+++ code/content.js
@@ -1,6 +1,7 @@
-var calendar_grid_selector = 'div[role="grid"]';
 var body = document.querySelector('body');
+var calendar_grid_selector = 'div[role="grid"]';
 var calendar_grid = document.querySelectorAll(calendar_grid_selector);
+var element_to_observe = document.querySelector('div[class="BXL82c"]');
 
 var disable_scroll = function () {
     for (var live_selector of document.querySelectorAll(calendar_grid_selector)) {
@@ -19,7 +20,7 @@ var mutation_breaks_scroll_blocker = function (mutation) {
     }
 };
 
-var calendar_observer = new MutationObserver(function (mutations) {
+var overlay_observer = new MutationObserver(function (mutations) {
     mutations.forEach(function (mutation) {
         if (mutation_breaks_scroll_blocker(mutation)) {
             disable_scroll();
@@ -27,12 +28,17 @@ var calendar_observer = new MutationObserver(function (mutations) {
     });
 });
 
+var calendar_observer = new MutationObserver(function (mutations) {
+    disable_scroll();
+});
+
 var observe_if_calendar_available = function () {
     if (!calendar_grid) {
         window.setTimeout(observe_if_calendar_available, 500);
         return;
     }
-    calendar_observer.observe(body, {attributes: true});
+    overlay_observer.observe(body, {attributes: true});
+    calendar_observer.observe(element_to_check, {childList: true});
 };
 
 observe_if_calendar_available();
