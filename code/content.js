f67dd259aa95b46b0e8ba4a2933adbdcab31ee00
Matt Knutson <scaffold.mk@gmail.com>
2018-10-01 12:42:46 -0500

    Since <body> is available at page load, we can register this handler right away.

--- code/content.js
+++ code/content.js
@@ -1,6 +1,5 @@
 var calendar_grid_selector = 'div[role="grid"]';
 var body = document.querySelector('body');
-var calendar_grid = document.querySelectorAll(calendar_grid_selector);
 
 var disable_scroll = function () {
     for (var live_selector of document.querySelectorAll(calendar_grid_selector)) {
@@ -26,19 +25,7 @@ var calendar_observer = new MutationObserver(function (mutations) {
         }
     });
 });
+calendar_observer.observe(body, {attributes: true});
 
-var observe_if_calendar_available = function () {
-    if (!calendar_grid) {
-        window.setTimeout(observe_if_calendar_available, 500);
-        return;
-    }
-    calendar_observer.observe(body, {attributes: true});
-};
-
-observe_if_calendar_available();
-
-// Disable scrolling once at startup.
-// Note that the data-viewfamily mutation doesn't occur when the page is loaded.
-// Rather, the data-viewfamily mutation only occurs when the user changes views.
-// Thus, we need to disable scrolling once when the page is initially loaded.
+// Disable scrolling once at startup just in case no mutations occur at page load.
 disable_scroll();
